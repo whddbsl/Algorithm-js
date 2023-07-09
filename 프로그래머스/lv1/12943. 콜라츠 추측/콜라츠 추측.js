f7d2ffdcd1d00b20536d180a830429d38711f435
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-10 00:19:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,14 @@
+function solution(num) {
+    for(i = 0; i < 500; i++){
+        if (num == 1){
+            return i;
+        }
+        else if(num%2 == 1 ){
+            num = (num *3) +1
+        }  
+        else if(num%2==0){
+            num = num/2
+        }   
+    }
+    return -1;
+}

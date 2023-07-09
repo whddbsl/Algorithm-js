bac7f010af0ed0d6267961248a337e79b20f600f
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-10 00:17:34 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,9 @@
+function solution(slice, n) {
+    var answer = 0;
+    if(n%slice == 0){
+        answer = n/slice
+    }else(
+        answer = Math.floor(n / slice) + 1
+    )
+    return answer;
+}

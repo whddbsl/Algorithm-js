beac83cdd5bd2f3451f48b1dec397601207338d2
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-11 02:13:37 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,9 +1,9 @@
 function solution(num, total) {
     var answer = [];
     let mid = total / num
-    let item = mid - (num-1)/2;
+    let first = mid - (num-1)/2;
     for (i = 0; i < num; i ++){
-        answer.push(item+i);
+        answer.push(first + i);
     }
     return answer;
 }

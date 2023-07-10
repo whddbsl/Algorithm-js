f6839d36b76707cbda73f92e8dbb1bc404f2fae5
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-11 01:57:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer = 0;
+    if((Math.sqrt(n) % 1 ) == 0){
+        answer = (Math.sqrt(n) + 1) ** 2;
+    }else{
+        answer = -1
+    }
+    return answer;
+}

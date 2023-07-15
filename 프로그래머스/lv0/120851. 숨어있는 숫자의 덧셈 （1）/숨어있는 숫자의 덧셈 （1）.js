666c036e7fc67be26f3385c637799894bf46ff2a
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-15 23:03:29 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let answer = 0;
+    for (let i = 0; i < my_string.length; i++) {
+        if (!isNaN(my_string[i])) {
+            answer += Number(my_string[i])
+        } 
+    } 
+    return answer;
+}

e9265d20156c2889cf69bb970a6fb7925fdecd87
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-15 23:12:57 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,9 +1,7 @@
 function solution(num_list, n) {
     let answer = 0;
-    for(i=0; i<num_list.length; i++){
-        if(num_list[i] === n){
-            answer = 1
-        }
+    if(num_list.includes(n)){
+        answer = 1;
     }
     return answer;
 }

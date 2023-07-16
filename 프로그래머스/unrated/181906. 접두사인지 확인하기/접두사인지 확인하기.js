eaafb2a8af6b91e57b7aba17ed80d9a3f1516ab7
whddbsl <130979302+whddbsl@users.noreply.github.com>
2023-07-16 23:29:43 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_prefix) {
+    let answer = 0;
+    if(my_string.startsWith(is_prefix)){
+        answer = 1;
+    }else{
+        answer = 0;
+    }
+    return answer;
+}
